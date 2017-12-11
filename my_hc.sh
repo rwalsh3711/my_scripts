0cb050dfe85ba230829bf0a78f720c622c71fa35
rwalsh3711 <rwalsh3711@gmail.com>
2017-12-11 14:07:55 -0600

    modified my_hc scripts

--- my_hc.sh
+++ my_hc.sh
@@ -6,18 +6,18 @@
 #
 
 if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
+   echo -e "This script must be run as root" 1>&2
    exit 1
 fi
 
 ## VARIABLES ##
 TMPFILE=/tmp/hc_server.$$.tmp
 
-echo "What is the name of the server or host file?"
+echo -e "What is the name of the server or host file?"
 
 read ANSWER
 
-echo "Is \""${ANSWER}"\" a (s)erver or a (f)ile?"
+echo -e "Is \""${ANSWER}"\" a (s)erver or a (f)ile?"
 printf "Enter (s) or (f): "
 read ANS
 if [ ${ANS} == "s" ]; then
@@ -25,7 +25,7 @@ if [ ${ANS} == "s" ]; then
 elif [ ${ANS} == "f" ]; then
         HOSTLIST=`cat ${ANSWER}`
 else 
-        echo "Invalid answer"
+        echo -e "Invalid answer"
         exit 0
 fi
 
@@ -35,7 +35,7 @@ fi
 # Function to check for filesystems greater than THRESHOLD
 _chk_fs() {
 THRESHOLD="85"
-echo "\t\t- FILESYSTEMS OVER ${THRESHOLD}% FULL -"	
+echo -e "\t\t- FILESYSTEMS OVER ${THRESHOLD}% FULL -"	
 EXCLUDE="/proc|/aha|Filesystem"
 
 	if [ "${OS}" == "AIX" ]; then
@@ -43,25 +43,25 @@ EXCLUDE="/proc|/aha|Filesystem"
 	elif [ "${OS}" == "Linux" ]; then
 		ssh ${HOST} "df -hP" |egrep -v "${EXCLUDE}" |awk {'print $5"\t"$6'} > ${TMPFILE}
 	else
-		echo "Unknown OS"
+		echo -e "Unknown OS"
 		continue
 	fi
 	cat ${TMPFILE} |while read LINE
 	do
-	 	PERC=`echo $LINE |awk {'print $1'} |cut -d"%" -f1`	
-		FS=`echo $LINE |awk {'print $2'}`	
+	 	PERC=`echo -e $LINE |awk {'print $1'} |cut -d"%" -f1`	
+		FS=`echo -e $LINE |awk {'print $2'}`	
 		if [ $PERC -gt $THRESHOLD ] 2>/dev/null; then 
-		echo $PERC%"\t"$FS
+		echo -e $PERC%"\t"$FS
 		fi
 	done 
 	rm -f ${TMPFILE}
-	echo ""
+	echo -e ""
 }
 
 # Function to check system memory
 _chk_mem() 
 {
-echo "\t\t- MEMORY STATUS -"
+echo -e "\t\t- MEMORY STATUS -"
 	if [[ $OS == "Linux" ]]; then
 		ssh ${HOST} "free -m" > ${TMPFILE}
 		MEM_TOTAL=`cat ${TMPFILE} |head -2 |tail -1 |awk {'print $2'}`
@@ -81,44 +81,44 @@ echo "\t\t- MEMORY STATUS -"
 MEM_USED_PERCENT=$(awk "BEGIN { pc=100*${MEM_CACHE}/${MEM_TOTAL}; i=int(pc); print (pc-i<0.5)?i:i+1 }")
 SWAP_USED_PERCENT=$(awk "BEGIN { pc=100*${SWAP_USED}/${SWAP_TOTAL}; i=int(pc); print (pc-i<0.5)?i:i+1 }")
 
-echo "Physical Memory is ${MEM_USED_PERCENT}% Used"
-echo "Swap Space is ${SWAP_USED_PERCENT}% Used"
-echo ""
+echo -e "Physical Memory is ${MEM_USED_PERCENT}% Used"
+echo -e "Swap Space is ${SWAP_USED_PERCENT}% Used"
+echo -e ""
 rm -f ${TMPFILE}
 }
 
 # Function to check system CPU
 _chk_cpu() {
-echo "\t\t- CPU STATUS (AVERAGE OVER 3 SECONDS) -" 
+echo -e "\t\t- CPU STATUS (AVERAGE OVER 3 SECONDS) -" 
 ssh ${HOST} "sar 1 3" |egrep "idle|Average"
-echo ""
+echo -e ""
 }
 
 # Function to report recent system error messages
 _chk_err() {
-echo "\t\t- RECENT SYSTEM ERRORS -"
+echo -e "\t\t- RECENT SYSTEM ERRORS -"
         if [ "${OS}" == "AIX" ]; then
                 ssh ${HOST} "errpt -T"PEND,PERM"" |tail -n5
         elif [ "${OS}" == "Linux" ]; then
                 ssh ${HOST} "cat /var/log/messages" |grep -i error|cut -c 1-100 |tail -n5
         else
-                echo "Unknown OS"
+                echo -e "Unknown OS"
                 continue
         fi
-echo ""
+echo -e ""
 }
 
 # ## MAIN SCRIPT ##
 
 for HOST in ${HOSTLIST}
 do
-echo ""
-echo "############"
-echo "# "${HOST}""
-echo "############"
+echo -e ""
+echo -e "############"
+echo -e "# "${HOST}""
+echo -e "############"
 
 	# Test if the server is reachable
-	echo "\t\t- PING RESULTS -"
+	echo -e "\t\t- PING RESULTS -"
 	ping -c 1 ${HOST} >/dev/null 2>&1
 		if [ $? = 0 ] ; then
 		printf "${HOST} is Reachable\n"
@@ -126,23 +126,23 @@ echo "############"
 		printf "${HOST} is Not Reachable\n"
 		continue
 		fi
-	echo ""
+	echo -e ""
 
 	# Check if root user can log into host
-	echo "\t\t- SSH RESULTS -"
-	ssh -o BatchMode=yes -o ConnectTimeout=5 ${HOST} "echo ok" >/dev/null 2>&1
+	echo -e "\t\t- SSH RESULTS -"
+	ssh -o BatchMode=yes -o ConnectTimeout=5 ${HOST} "echo -e ok" >/dev/null 2>&1
 		if [ $? != 0 ] ; then
-		echo "SSH Keys Not Installed"
+		echo -e "SSH Keys Not Installed"
 		continue
 		else
-		echo "SSH Keys Installed Correctly"
-		echo ""
+		echo -e "SSH Keys Installed Correctly"
+		echo -e ""
 		fi
 
 	# Collect the OS Level
 	OS=`ssh ${HOST} "uname -s"`
 	if [ $OS != AIX ] && [ $OS != Linux ]; then
-                echo "${HOST} is running ${OS}.  I refuse to work with ${OS}.  Please go pound sand."
+                echo -e "${HOST} is running ${OS}.  I refuse to work with ${OS}.  Please go pound sand."
                 continue
         fi
 
